78ed5a986b83b85c0772064e1c15f2b26cf5b270
Roberto <roberto.amato.11.93@gmail.com>
2020-03-22 17:59:08 +0100

    - added a package com.webapp.cardatabase.service 	- added a class UderDetailServiceImpl that implements UserDetailService
    	In class UserDetailService override the method
    loadUserByUsername(String username) that retrieve a user from db with
    specific username and create a USer SEcurity Spring.

--- src/main/java/com/webapp/cardatabase/domain/OwnerRepository.java
+++ src/main/java/com/webapp/cardatabase/domain/OwnerRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 	
 	public Optional<Owner> findById(Long id);
--- src/main/java/com/webapp/cardatabase/domain/UserRepository.java
+++ src/main/java/com/webapp/cardatabase/domain/UserRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
 	public Optional<User> findById(Long id);
