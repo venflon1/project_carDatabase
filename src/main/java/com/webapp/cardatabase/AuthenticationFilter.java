e8f783390f48ed827b4d1faa36c3a78fa5e5f574
Roberto <roberto.amato.11.93@gmail.com>
2020-03-24 19:59:20 +0100

    add class AuthenticationFilter.java

---
+++ src/main/java/com/webapp/cardatabase/AuthenticationFilter.java
@@ -0,0 +1,45 @@
+package com.webapp.cardatabase;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.GenericFilter;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.jboss.logging.Logger;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.webapp.cardatabase.service.AuthenticationService;
+
+
+/**
+ * This class extends GenericFilterBean, which is a generic superclass for any type of filter.
+ * This class will handle authentication in all other endpoints except '/login'.
+ * 
+ * The AuthenticationFilter uses the addAuthentication method from our service class 
+ * to get a token from the request Authorization header
+ * */
+public class AuthenticationFilter extends GenericFilter{
+
+	private static Logger log = Logger.getLogger(AuthenticationFilter.class);
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+			throws IOException, ServletException {
+		log.info("doFilter(ServletRequest request, ServletResponse response, FilterChain chain) - START");
+		
+		// AuthenticationServise is a class create before in package com.webapp.cardatabase.service
+		// use a static method getAuthentication of class AuthenticationService
+		Authentication auth = AuthenticationService.getAuthentication((HttpServletRequest)request);
+		
+		SecurityContextHolder.getContext().setAuthentication(auth);
+		
+		filterChain.doFilter(request, response);
+		log.info("doFilter(ServletRequest request, ServletResponse response, FilterChain chain) - END");
+	}
+
+	
+}
